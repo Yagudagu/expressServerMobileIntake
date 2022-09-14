271ad95fecafbcd1ac0d1b205ee9fedeb3c447a7
Jacob Martin <Jacobhmartian@gmail.com>
2022-09-14 17:58:55 -0600

    again

--- controllers/authController.js
+++ controllers/authController.js
@@ -67,6 +67,7 @@ exports.login = async (req, res, next) => {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     //secure: true,
     httpOnly: true,
+    sameSite: "none",
   });
 
   res.status(200).json({
