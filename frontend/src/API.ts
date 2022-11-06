290f31e297a4050cd536639e7bce2a3c104b8b55
Jacob Martin <Jacobhmartian@gmail.com>
2022-11-06 13:24:13 -0700

    changed to mobile url

--- frontend/src/API.ts
+++ frontend/src/API.ts
@@ -1,7 +1,7 @@
 import { HouseholdWith } from "./models";
 import axios from "axios";
 
-const rootUrl = "https://jacobmartinworld.com";
+const rootUrl = "https://mobileclothes.org";
 
 const API = {
   fetchHouseholds: async () => {
